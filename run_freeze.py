e302204e7e4ec53aae37c268e04644d85505c91a
larslehrem <lars.lovberg@gmail.com>
2020-08-01 20:45:02 +0200

    Fixed config

--- run_freeze.py
+++ run_freeze.py
@@ -1,7 +1,7 @@
 from app import create_app
 import flask_frozen
 
-freezer = flask_frozen.Freezer(create_app(config_file_name = dev_config))
+freezer = flask_frozen.Freezer(create_app(config_file_name = 'dev_config.py'))
 
 if __name__ == '__main__':
     freezer.freeze()
