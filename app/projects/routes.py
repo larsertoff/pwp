d4c9352f2f2d13c7868049ccaf04622415820d81
larslehrem <lars.lovberg@gmail.com>
2020-08-17 00:44:58 +0200

    Small change

--- app/projects/routes.py
+++ app/projects/routes.py
@@ -56,27 +56,23 @@ def projects_specific(repo=None):
     gitql = GithubQuery()
 
     query_specific_repo = string.Template("""
-            repository(owner: "${username}", name: "${repository}") {
-                    name
-                    createdAt
-                    url
-                    descriptionHTML
-                    description
-                    updatedAt
-                    primaryLanguage {
-                    name
-                    }
-                }
-            }""")
+            query {
+            repository(name:"${repository}", owner:"${username}"){
+                        id
+                        name
+                        description
+                        }
+                    }""")
 
     
     query_string = str(query_specific_repo.substitute(username=gitql.login, repository = repo))
     specific_repository = gitql.run_query(query_string)
-    print(specific_repository)
 
-    query_blog_post_folder = string.Template("""
+    specific_repository = specific_repository['data']['repository']
+
+    query_blog_post_folder = string.Template("""query{
     repository(owner: "${username}", name: "${repository}" ) {
-       filename: object(expression: "master:example/") {
+       filename: object(expression: "master:blog_post/") {
       ... on Tree {
         entries {
           name
@@ -84,10 +80,15 @@ def projects_specific(repo=None):
           }
           }
           }
+          }
     """)
     query_string = str(query_blog_post_folder.substitute(username=gitql.login, repository = repo))
-    specific_file = gitql.run_query(query_string)
-    print(specific_file)
+    blog_post_folder = gitql.run_query(query_string)
+    
+    blog_post_folder = blog_post_folder['data']['repository']['filename']
+
+    if blog_post_folder is None:
+        print("Something is wrong with the query")
 
     # Look for a file named main.json
     query_specific_file=string.Template("""{
@@ -100,13 +101,21 @@ def projects_specific(repo=None):
                 }
             }
         }""")
-    print(repo)
     query_string = str(query_specific_file.substitute(username=gitql.login, repository = repo))
-    print(query_string)
     specific_file = gitql.run_query(query_string)
 
-    print(specific_file)
+    specific_file = specific_file['data']['repository']['object']
 
     # Get info on specific repo present nicely
 
-    return flask.render_template('projects_specific.html', repo=repo)
+    print(specific_repository)
+    print(blog_post_folder)
+    print(specific_file)
+
+
+    # Ensure some healty default if the queries are off
+    if [x for x in (specific_repository, blog_post_folder, specific_file) if x is None]:
+        # Fix the queries with some default, il rather do it here than in the template
+        pass
+
+    return flask.render_template('projects_specific.html', repo=repo, specific_file = specific_file)
